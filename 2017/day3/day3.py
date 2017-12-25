0977932dee668c959a7073fbb17b36b7a2332fb6
Alexander Joseph Trelore <alextrelore@gmail.com>
2017-12-25 00:54:58 +0000

    Day 3 - Python - explanation

--- 2017/day3/day3.py
+++ 2017/day3/day3.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
-from math import ceil, floor, sqrt
+from math import ceil, floor
 
 
+# Algorithm relies on square numbers. Designed for efficiency this algorithm avoids costly operations such as sqrt
+# First, determine the floor of the root of the number-1. i.e. 12 -> 3, 21 -> 4, 1 -> 0
+# Second, determine the numbers between i**2 and (i+1)**2 with the smallest manhattan distance,
+# i.e. 1, 2, 11, 28 horizontally and 1, 4, 15, 34 vertically
+# Third, find the minimum between the number and the v/h numbers and add that to the distance to the h/v number
 def manhattan_distance(number):
     i = 0
     while (i+1)**2 < number:
