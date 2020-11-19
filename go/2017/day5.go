73887b1b830447ad19afe6e2d7b6701f7eef47cc
Alexander J Trelore <alextrelore@gmail.com>
2020-11-19 22:37:07 +0000

    day 5

--- go/2017/day5.go
+++ go/2017/day5.go
@@ -1,41 +1,41 @@
 package twentyseventeen
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/trelore/advent-of-code/go/io"
-)
+// 	"github.com/trelore/advent-of-code/go/io"
+// )
 
-func Day5Solve(inputFile string) {
-	input, err := io.ReadFileIntSlice(inputFile)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	p1 := day5(input)
-	p2 := day5Bonus(input)
-	fmt.Printf("d5p1 = %d\nd5p2 = %d\n", p1, p2)
-}
+// func Day5Solve(inputFile string) {
+// 	input, err := io.ReadFileIntSlice(inputFile)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	p1 := day5(input)
+// 	p2 := day5Bonus(input)
+// 	fmt.Printf("d5p1 = %d\nd5p2 = %d\n", p1, p2)
+// }
 
-func day5(input []int) int {
-	counter := 0
-	for i := 0; 0 <= i && i < len(input); counter++ {
-		input[i]++
-		i = i + input[i] - 1
-	}
-	return counter
-}
+// func day5(input []int) int {
+// 	counter := 0
+// 	for i := 0; 0 <= i && i < len(input); counter++ {
+// 		input[i]++
+// 		i = i + input[i] - 1
+// 	}
+// 	return counter
+// }
 
-func day5Bonus(input []int) int {
-	counter := 0
-	for i := 0; 0 <= i && i < len(input); counter++ {
-		if input[i] >= 3 {
-			input[i]--
-			i = i + input[i] + 1
-		} else {
-			input[i]++
-			i = i + input[i] - 1
-		}
-	}
-	return counter
-}
+// func day5Bonus(input []int) int {
+// 	counter := 0
+// 	for i := 0; 0 <= i && i < len(input); counter++ {
+// 		if input[i] >= 3 {
+// 			input[i]--
+// 			i = i + input[i] + 1
+// 		} else {
+// 			input[i]++
+// 			i = i + input[i] - 1
+// 		}
+// 	}
+// 	return counter
+// }
--- go/main.go
+++ go/main.go
@@ -9,11 +9,11 @@ func main() {
 	twentyseventeen.Day2Solve("2017/day2.txt")
 	twentyseventeen.Day3Solve("2017/day3.txt")
 	twentyseventeen.Day4Solve("2017/day4.txt")
-	twentyseventeen.Day5Solve("2017/day5.txt")
-	twentyseventeen.Day6Solve("2017/day6.txt")
-	// get original input -> put into file and conform to new standard
-	// day6.Day6([]int{5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6})
-	// day6.Day6Bonus([]int{5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6})
+	// twentyseventeen.Day5Solve("2017/day5.txt")
+	// twentyseventeen.Day6Solve("2017/day6.txt")
+	// // get original input -> put into file and conform to new standard
+	// // day6.Day6([]int{5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6})
+	// // day6.Day6Bonus([]int{5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6})
 	twentyseventeen.Day7Solve("2017/day7.txt")
-	twentyseventeen.Day8Solve("2017/day8.txt")
+	// twentyseventeen.Day8Solve("2017/day8.txt")
 }
